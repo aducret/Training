96c2620b15b407241091bc6cae6451ee054d58da
Argentino Ducret <argentinoducret@Woloxs-Mac-mini-4.local>
2015-03-25 16:26:19 -0300

    first commit

---
+++ Podfile
@@ -0,0 +1,4 @@
+source 'https://github.com/CocoaPods/Specs.git'
+pod 'AFNetworking', '~> 2.0'
+pod 'ObjectiveSugar', '~> 0.5'
+workspace 'Training'
---
+++ Training/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Training
+//
+//  Created by Argentino Ducret on 3/25/15.
+//  Copyright (c) 2015 Wolox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
