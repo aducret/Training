1fbed6e062359ed38259b7b7d70ea8a67bdfdde8
aducret <argentino.ducret@wolox.com.ar>
2015-03-30 11:21:38 -0300

    Validaciones en Log In.
    Se agregaron las validaciones requeridas de email y password en la vista
    de Log In, se estan mostrando con un toast.

--- Training/LoginViewController.m
+++ Training/LoginViewController.m
@@ -8,6 +8,7 @@
 
 #import "LoginViewController.h"
 #import <Parse/Parse.h>
+#import <UIView+Toast.h>
 
 
 @interface LoginViewController()
@@ -19,6 +20,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *logInButton;
 @property (weak, nonatomic) IBOutlet UIButton *singUpButton;
 @property (weak, nonatomic) IBOutlet UILabel *termsAndConditionsLabel;
+
+- (BOOL) validateEmail: (NSString *) candidate;
 
 @end
 
@@ -50,14 +53,42 @@
 }
 
 - (IBAction)logIn:(id)sender {
-    [PFUser logInWithUsernameInBackground:self.emailTextField.text password:self.passwordTextField.text block:^(PFUser *user, NSError *error) {
-        if (user) {
-            NSLog(@"login");
-        } else {
-            //error
-            NSLog(@"error");
+    
+    if( self.emailTextField.text.length > 0 && self.passwordTextField.text.length > 0 ){
+        
+        if( ![self validateEmail: self.emailTextField.text] ){
+            [self.view makeToast:NSLocalizedString(@"invalidEmail", nil)];
+            return;
         }
-    }];
+
+        //Loggin
+        [PFUser logInWithUsernameInBackground:self.emailTextField.text password:self.passwordTextField.text block:^(PFUser *user, NSError *error) {
+            if (user) {
+                NSLog(@"login");
+            } else {
+                //error
+                [self.view makeToast:NSLocalizedString(@"invalidLogIn", nil)];
+            }
+        }];
+    }
+    else {
+        //All fields empty
+        [self.view makeToast:NSLocalizedString(@"allRequired", nil)];
+    }
+}
+
+- (BOOL) validateEmail: (NSString *) candidate {
+    NSString *emailRegex =
+    @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
+    @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+    @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
+    @"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
+    @"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
+    @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
+    @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES[c] %@", emailRegex];
+    
+    return [emailTest evaluateWithObject:candidate];
 }
 
 @end
