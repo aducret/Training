e1dbc7749c02cc81a6b131bb5610f8368c539e9f
aducret <argentino.ducret@wolox.com.ar>
2015-03-30 13:26:57 -0300

    Agregue la clase RegexValidations.
    Para no repetir codigo, pase la validacion de email a esta clase.

---
+++ Training/Helpers/RegexValidations.h
@@ -0,0 +1,15 @@
+//
+//  RegexValidations.h
+//  Training
+//
+//  Created by Argentino Ducret on 3/30/15.
+//  Copyright (c) 2015 Wolox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RegexValidations : NSObject
+
++ (BOOL) validateEmail: (NSString *) candidate;
+
+@end
