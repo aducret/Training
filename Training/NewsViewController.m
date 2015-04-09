b209dacf26bc8a2bb910b97d4a2875c0685ecbf7
aducret <argentino.ducret@wolox.com.ar>
2015-04-09 18:27:33 -0300

    Se agrego un refresh controller a la vista de News y se agrego notificacion de errores.
    Cuando se hace refresh se vuelva a cargar desde cero todos los items(si ya habia cargados 20
    y se borra todos los items, volviendo a cargar los 7 iniciales). Se hace esto para poder cargar las
    news nuevas.
    Si ocurre algun error con el servidor se notifica con una imagen y con un toast (no se esta renderizando
    bien la imagen).

--- Training/NewsViewController.m
+++ Training/NewsViewController.m
@@ -23,9 +23,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.items = [[NSMutableArray alloc]init];
-    [self setEmptyList];
-    [self loadInitData];
+    [self initRefreshControl];
+    [self initItemsArray];
+    [self setEmptyListImage];
+    [self loadData];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -42,8 +43,10 @@
         NSArray * nib = [[NSBundle mainBundle] loadNibNamed:@"TableCell" owner:self options:nil];
         cell = [nib objectAtIndex:0];
     }
-    Comment * comment = [self.items objectAtIndex:indexPath.row];
-    [self initCell:cell withComment:comment];
+    if([self.items count] > 0) {
+        Comment * comment = [self.items objectAtIndex:indexPath.row];
+        [self initCell:cell withComment:comment];
+    }
     
     return cell;
 }
@@ -63,7 +66,14 @@
         [self launchReload];
     }
 }
+
 #pragma mark - Private methods
+
+- (void)refresh:(id)sender {
+    [self.items removeAllObjects];
+    [self launchReload];
+    [(UIRefreshControl *)sender endRefreshing];
+}
 
 - (void) initCell: (TableCell *) cell withComment: (Comment *) comment {
     cell.textLabel.text = comment.text;
@@ -72,10 +82,11 @@
     cell.dateLabel.text = [comment.creationDate timeAgo];
 }
 
-- (void)loadInitData {
+- (void)loadData {
     PFQuery * query = [PFQuery queryWithClassName:@"Comment"];
     query.limit = 7;
     query.skip = [self.items count];
+    [query orderByDescending:@"creationDate"];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if (!error && objects.count > 0) {
             NSLog(@"Successfully retrieved %ld scores.", objects.count);
@@ -83,19 +94,37 @@
             self.tableView.backgroundView = nil;
             [self.tableView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
         } else {
-            [self.view makeToast:NSLocalizedString(@"", nil)];
+            [self.view makeToast:NSLocalizedString(@"serverError", nil)];
+            [self setServerErrorImage];
         }
     }];
 }
 
-- (void)setEmptyList {
+- (void)setEmptyListImage {
     UIImage * image = [UIImage imageNamed:@"first"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
     self.tableView.backgroundView = imageView;
 }
 
+- (void)setServerErrorImage {
+    UIImage * image = [UIImage imageNamed:@"i-error"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    self.tableView.backgroundView = imageView;
+
+}
+
 - (void)launchReload {
-    [self loadInitData];
+    [self loadData];
+}
+
+- (void)initRefreshControl {
+    UIRefreshControl * refresh = [[UIRefreshControl alloc] init];
+    [refresh addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
+    [self setRefreshControl:refresh];
+}
+
+- (void)initItemsArray {
+    self.items = [[NSMutableArray alloc]init];
 }
 
 @end
