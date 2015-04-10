7391997c018d5dc351832da6202dbf4eab42d6ee
aducret <argentino.ducret@wolox.com.ar>
2015-04-10 14:56:05 -0300

    Se creo la vista para crear comentarios.

---
+++ Training/CreateCommentViewController.h
@@ -0,0 +1,17 @@
+//
+//  CreateCommentViewController.h
+//  Training
+//
+//  Created by Argentino Ducret on 4/10/15.
+//  Copyright (c) 2015 Wolox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateCommentViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton * ImageButton;
+@property (weak, nonatomic) IBOutlet UITextField * titleTextField;
+@property (weak, nonatomic) IBOutlet UITextField * descriptionTextField;
+
+@end
---
+++ Training/CreateCommentViewController.m
@@ -0,0 +1,44 @@
+//
+//  CreateCommentViewController.m
+//  Training
+//
+//  Created by Argentino Ducret on 4/10/15.
+//  Copyright (c) 2015 Wolox. All rights reserved.
+//
+
+#import "CreateCommentViewController.h"
+
+@interface CreateCommentViewController ()
+
+@end
+
+@implementation CreateCommentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (IBAction)setImage:(id)sender {
+    
+}
+
+- (IBAction)saveComment:(id)sender {
+    
+}
+
+- (IBAction)cancel:(id)sender {
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
